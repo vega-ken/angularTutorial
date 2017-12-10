d8423a51c138c8bab6e517b7b4759a2f1bc0ca09
Alexander Vega <vega.ken.developer@gmail.com>
2017-12-09 21:36:53 -0500

    viendo los tipos de variables que se pueden definir en los componentes

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -3,34 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sandbox',
   template: `
-    <h1>{{name}} is {{age}} years old.</h1>
-    <h2>My name is {{person.firstName}} {{person.lastName}}.</h2>
-
-    <p>Esta parte es llamada con una funcion: {{ showAge() }} </p>
+    <h1>Hello {{ name }}.</h1>
   `
 })
 
 export class SandboxComponent{ // tu clase debe hacer match con el nombre del archivo
-  name = 'John Doe'; // propiedad
-  age = 35; // es raro que no se tenga que poner let o var
+  name:string = 'John Doe';
+  age:number = 45;
+  hasChildren:boolean = true;
 
-  person = {
-    firstName: 'Steve',
-    lastName: 'Smith'
-  };
+  city:any = 'Boston';
+  myNumbersArray:number[] = [1,2,3];
+  myStringsArray:string[] = ['hola', 'mundo'];
+
+  myAnyArray:any[] = ['hello', 234];
+
+  myTuple: [string, number] = ['hello', 3]; // mas especifico
+
+  unusable:void = undefined;
+  u:undefined = undefined;
+  n:null = null; // o undefined;
+
 
   constructor(){ // se ejecuta cuando el componente se incializa
-    console.log('constructor se ejecuto');
-    this.age = 40;
-    this.hasBirthDay();
+    this.myNumbersArray = [2,3];
   }
 
-  hasBirthDay(){
-    this.age += 1;
-  }
-
-  showAge(){
-    return this.age;
-  }
-  
 }
