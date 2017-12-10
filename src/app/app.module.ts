314d29cddede26a560c240771fe792a9947d709d
Alexander Vega <vega.ken.developer@gmail.com>
2017-12-09 20:54:02 -0500

    explicado el ngmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'; // se encarga de el display de la aplicacion en el browser
 import { NgModule } from '@angular/core';
 
-
+// cada componente creado tendra que estar aqui
 import { AppComponent } from './app.component';
 
 
