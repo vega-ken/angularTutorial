6db220c95e21771dfcc42da00e81cf3b821f70eb
Alexander Vega <vega.ken.developer@gmail.com>
2017-12-09 22:57:45 -0500

    for loops con el ngFor

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -3,15 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sandbox',
   templateUrl: `./sandbox.component.html`,
-  styleUrls : [`./sandbox.component.css`]
-  /*styles: [`
-    .special {
-      color: green;
-    }
-  `]*/
+  styleUrls: [`./sandbox.component.css`]
+
 })
 
-export class SandboxComponent{ // tu clase debe hacer match con el nombre del archivo
+export class SandboxComponent { // tu clase debe hacer match con el nombre del archivo
 
- 
+  people = ['Rick', 'Daryl', 'Carl', 'Glen'];
+
+  people2 = [ 
+    {
+      firstName: 'Rick',
+      lastName: 'Grimes'
+    },
+    {
+      firstName: 'Daryl',
+      lastName: 'Dixon'
+    },
+    {
+      firstName: 'Carl',
+      lastName: 'Grimes'
+    },
+    {
+      firstName: 'Glen',
+      lastName: 'Rhee'
+    }
+  ];
+
+  constructor(){
+    this.people[2] = 'Carol';
+  }
+
 }
