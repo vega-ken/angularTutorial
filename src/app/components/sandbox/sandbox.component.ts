5597f229122f017117163f6eb68c56091840e342
Alexander Vega <vega.ken.developer@gmail.com>
2017-12-11 16:20:52 -0500

    terminando la validacion de forms. tambien submission de forms agregado

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -14,4 +14,16 @@ export class SandboxComponent { // tu clase debe hacer match con el nombre del a
     email: '',
     phone: ''
   }
+
+  onSubmit({value, valid}){
+    // recuerda que aqui le envian 'f' que trae 2 cosas como un objeto
+      //value representa el objeto del form (los campos que han sido rellenados)
+      //valid representa la validacion de los campos
+    if(valid){
+      console.log(value);
+    }
+    else{
+      console.log('Form is invalid');
+    }
+  }
 }
