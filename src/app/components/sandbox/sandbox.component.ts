ca4de58f92dcb79cb2281dbf39baf0a5267b4dc7
Alexander Vega <vega.ken.developer@gmail.com>
2017-12-10 23:50:33 -0500

    added ngStyle

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
 })
 
 export class SandboxComponent { // tu clase debe hacer match con el nombre del archivo
-  
+  isSpecial = true;
+  canSave = true;
+  currentStyles = {};
+
+  constructor(){
+    this.setCurrentStyles();
+  }
+
+  setCurrentStyles(){
+    this.currentStyles = {
+      'font-style' : this.canSave ? 'italic' : 'normal',
+      'font-size' : this.isSpecial ? '24px' : '12px'
+    }
+  }
 }
