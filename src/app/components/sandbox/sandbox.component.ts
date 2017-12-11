33ae89038a5ce054730443604c9af7052a447f25
Alexander Vega <vega.ken.developer@gmail.com>
2017-12-10 23:35:38 -0500

    ?

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -8,19 +8,5 @@ import { Component } from '@angular/core';
 })
 
 export class SandboxComponent { // tu clase debe hacer match con el nombre del archivo
-  isSpecial:boolean = true;
-  canSave:boolean = true;
-
-  currentClasses = {};
-
-  constructor(){
-    this.setCurrentClasses();
-  }
-
-  setCurrentClasses(){
-    this.currentClasses = {
-      saveable:this.canSave,
-      special2:this.isSpecial
-    }  
-  }
+  
 }
