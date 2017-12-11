4927642a98142f6655a40b5bd691a200bb353c81
Alexander Vega <vega.ken.developer@gmail.com>
2017-12-11 13:33:36 -0500

    cambiando propiedades de componentes a traves de eventos

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -9,11 +9,12 @@ import { Component } from '@angular/core';
 
 export class SandboxComponent { // tu clase debe hacer match con el nombre del archivo
   
-  fireEvent(e , greeting){
-    console.log('event ocurred!');
-    console.log(e.currentTarget);
-    console.log(e.target);
-    console.log(e.type);
-    console.log(greeting);
+  text:string = 'Hello world';
+  value:boolean = true;
+
+  changeValue(){
+    //this.value = false;
+    this.value = !this.value; // for toogling
   }
+ 
 }
