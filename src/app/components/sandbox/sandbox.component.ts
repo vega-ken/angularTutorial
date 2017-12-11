c2b6c9d66b3f6b3eb7cfe565ab96c1bc03bcecdb
Alexander Vega <vega.ken.developer@gmail.com>
2017-12-11 16:03:37 -0500

    validacion de forms en angular

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -9,12 +9,9 @@ import { Component } from '@angular/core';
 
 export class SandboxComponent { // tu clase debe hacer match con el nombre del archivo
   
-  name:string = "";
-  users:string[] = ['Jhon doe', 'Marie Jane', 'Smith smith'];
-
-  onSubmit(){ // evento lanzado por el form
-    this.users.push(this.name);
-    this.name = ""; // como esta propiedad esta enlazada con el UI, ya no tienes que llegar al elemento del html. solo cambiar la propiedad!
+  user = {
+    name: '',
+    email: '',
+    phone: ''
   }
-
 }
